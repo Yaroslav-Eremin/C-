a32b9c04f3b995705366d8bebfb3d1ddea60a0f8
Ярослав <104647913+Yaroslav-Eremin@users.noreply.github.com>
2022-05-15 12:37:47 +0300

    Add files via upload

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+	srand(time(0));
+	int a, b, c = 0, d, t = 0;
+	cout << "borders:";
+	cin >> a >> b;
+	d = (rand() % b) + a;
+	while (t != d)
+	{
+		c++;
+		t = 0;
+		cin >> t;
+		if (t<d)
+		{
+			cout << "more"<<endl;
+		}
+		if (t>d)
+		{
+			cout << "smaller"<<endl;
+		}
+		if (t==d)
+		{
+			cout << "guessed right"<<c;
+		}
+	}
+	return 0;
+}
